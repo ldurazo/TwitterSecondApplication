1c1dafb2b88c0fcf0743d19abe17674fce501996
ldurazo <ldurazo@nearsoft.com>
2014-05-29 15:56:15 -0700

    Added profile picture

--- app/src/main/java/com/internship/twittersecondapplication/app/ListAdapterUpdater.java
+++
@@ -1,8 +0,0 @@
-package com.internship.twittersecondapplication.app;
-
-/**
- * Created by ldurazo on 5/28/2014.
- */
-public interface ListAdapterUpdater {
-    public void updateListAdapter(String jsonResponse);
-}
